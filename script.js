d092a93c7ab17d4e5dd308d5dd333f459e2c9b8a
Aditya Shendkar <aditya.shendkar03@gmail.com>
2024-01-12 19:27:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var myDiv = document.getElementById("myDiv");
+
+myDiv.style.backgroundColor = "yellow";
+myDiv.style.color = "blue";
+myDiv.style.margin = "20px";
+myDiv.style.padding = "10px";
+myDiv.style.fontSize = "18px";
+myDiv.style.fontWeight = "bold";
+myDiv.style.height = "200px";
+myDiv.style.width = "300px";
